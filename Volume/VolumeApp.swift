773b52ae1c2eb60ba5a5cb7905e5e889d6aef1d3
Ahmadreza <lovlyboy@gmail.com>
2023-07-31 11:35:25 +0330

    Initial Commit

---
+++ Volume/VolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeApp.swift
+//  Volume
+//
+//  Created by Ahmadreza on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct VolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
